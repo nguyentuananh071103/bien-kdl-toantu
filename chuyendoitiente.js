abe12ec23d81fc91c58c125021c96afcb4001cd0
nguyentuananh071103 <nguyenngocbao13579@gmail.com>
2021-09-12 21:20:31 +0700

    thuc hanh va bai tap

---
+++ chuyendoitiente.js
@@ -0,0 +1,7 @@
+function convertMoney() {
+    let money = document.getElementById('money').value;
+    let from = document.getElementById('in').value;
+    let to = document.getElementById('out').value;
+    let tien = money * to / from
+    document.getElementById('kq').innerHTML = "kết quả là:" + tien
+}
